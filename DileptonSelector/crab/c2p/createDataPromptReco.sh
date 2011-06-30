ba28865d31f6b65c25e1f7b972c32e8fa643e4b4
thea <>
2011-06-30 14:26:35 +0000

    added summary=1

--- DileptonSelector/crab/c2p/createDataPromptReco.sh
+++ DileptonSelector/crab/c2p/createDataPromptReco.sh
@@ -1,9 +1,9 @@
 nJobs=40
 csvFile="$HOME/higgsWW/samples/R42X_S1_V04_S2_V00_S3_V00.csv"
-optArgs="useLumi=$HOME/higgsWW/lumi/currentLumi.json"
+optArgs="summary=1 useLumi=$HOME/higgsWW/lumi/currentLumi.json"
 
-crab2prawn.py -c $csvFile -j $nJobs -t ../prawn_step2.sh -o /shome/thea/higgsWW/Summer11/jobs -w ../../ -i 84 -a "$optArgs triggerFilter=singleMu" -g 2011:PromptRecov4:42X:data:singleMu
-crab2prawn.py -c $csvFile -j $nJobs -t ../prawn_step2.sh -o /shome/thea/higgsWW/Summer11/jobs -w ../../ -i 85 -a "$optArgs triggerFilter=doubleEl" -g 2011:PromptRecov4:42X:data:doubleEl 
-crab2prawn.py -c $csvFile -j $nJobs -t ../prawn_step2.sh -o /shome/thea/higgsWW/Summer11/jobs -w ../../ -i 86 -a "$optArgs triggerFilter=doubleMu" -g 2011:PromptRecov4:42X:data:doubleMu 
-crab2prawn.py -c $csvFile -j $nJobs -t ../prawn_step2.sh -o /shome/thea/higgsWW/Summer11/jobs -w ../../ -i 87 -a "$optArgs triggerFilter=muEg"     -g 2011:PromptRecov4:42X:data:muEg 
+crab2prawn.py -c $csvFile -j $nJobs -t ../prawn_step2.sh -o /shome/thea/higgsWW/Summer11/jobs -w ../../ -i 84 -a "$optArgs dataPath=singleMu" -g 2011:PromptRecov4:42X:data:singleMu
+crab2prawn.py -c $csvFile -j $nJobs -t ../prawn_step2.sh -o /shome/thea/higgsWW/Summer11/jobs -w ../../ -i 85 -a "$optArgs dataPath=doubleEl" -g 2011:PromptRecov4:42X:data:doubleEl 
+crab2prawn.py -c $csvFile -j $nJobs -t ../prawn_step2.sh -o /shome/thea/higgsWW/Summer11/jobs -w ../../ -i 86 -a "$optArgs dataPath=doubleMu" -g 2011:PromptRecov4:42X:data:doubleMu 
+crab2prawn.py -c $csvFile -j $nJobs -t ../prawn_step2.sh -o /shome/thea/higgsWW/Summer11/jobs -w ../../ -i 87 -a "$optArgs dataPath=muEg"     -g 2011:PromptRecov4:42X:data:muEg 
 pwn_Update.py -n -g PromptRecov4:data queue=short.q,all.q
