2cf60dffd34d325e37a688040f643fb2c963227f
thea <>
2011-06-30 15:37:20 +0000

    another typo

--- DileptonSelector/crab/c2p/createDataPromptReco.sh
+++ DileptonSelector/crab/c2p/createDataPromptReco.sh
@@ -5,5 +5,5 @@ optArgs="summary=1 useLumi=$HOME/higgsWW/lumi/currentLumi.json"
 crab2prawn.py -c $csvFile -j $nJobs -t ../prawn_step2.sh -o /shome/thea/higgsWW/Summer11/jobs -w ../../ -i 84 -a "$optArgs dataPath=singleMu" -g 2011:PromptRecov4:42X:data:singleMu
 crab2prawn.py -c $csvFile -j $nJobs -t ../prawn_step2.sh -o /shome/thea/higgsWW/Summer11/jobs -w ../../ -i 85 -a "$optArgs dataPath=doubleEl" -g 2011:PromptRecov4:42X:data:doubleEl 
 crab2prawn.py -c $csvFile -j $nJobs -t ../prawn_step2.sh -o /shome/thea/higgsWW/Summer11/jobs -w ../../ -i 86 -a "$optArgs dataPath=doubleMu" -g 2011:PromptRecov4:42X:data:doubleMu 
-crab2prawn.py -c $csvFile -j $nJobs -t ../prawn_step2.sh -o /shome/thea/higgsWW/Summer11/jobs -w ../../ -i 87 -a "$optArgs dataPath=muEg"     -g 2011:PromptRecov4:42X:data:muEg 
+crab2prawn.py -c $csvFile -j $nJobs -t ../prawn_step2.sh -o /shome/thea/higgsWW/Summer11/jobs -w ../../ -i 87 -a "$optArgs dataPath=muEG"     -g 2011:PromptRecov4:42X:data:muEG 
 pwn_Update.py -n -g PromptRecov4:data queue=short.q,all.q
--- DileptonSelector/crab/c2p/createDataReReco.sh
+++ DileptonSelector/crab/c2p/createDataReReco.sh
@@ -5,5 +5,5 @@ optArgs="summary=1 useLumi=$HOME/higgsWW/lumi/currentLumi.json"
 crab2prawn.py -c $csvFile -j $nJobs -t ../prawn_step2.sh -o /shome/thea/higgsWW/Summer11/jobs -w ../../ -i 90 -a "$optArgs dataPath=singleMu" -g 2011:ReRecoMay10:42X:data:singleMu
 crab2prawn.py -c $csvFile -j $nJobs -t ../prawn_step2.sh -o /shome/thea/higgsWW/Summer11/jobs -w ../../ -i 91 -a "$optArgs dataPath=doubleMu" -g 2011:ReRecoMay10:42X:data:doubleMu 
 crab2prawn.py -c $csvFile -j $nJobs -t ../prawn_step2.sh -o /shome/thea/higgsWW/Summer11/jobs -w ../../ -i 92 -a "$optArgs dataPath=doubleEl" -g 2011:ReRecoMay10:42X:data:doubleEl 
-crab2prawn.py -c $csvFile -j $nJobs -t ../prawn_step2.sh -o /shome/thea/higgsWW/Summer11/jobs -w ../../ -i 93 -a "$optArgs dataPath=muEg"     -g 2011:ReRecoMay10:42X:data:muEg 
+crab2prawn.py -c $csvFile -j $nJobs -t ../prawn_step2.sh -o /shome/thea/higgsWW/Summer11/jobs -w ../../ -i 93 -a "$optArgs dataPath=muEG"     -g 2011:ReRecoMay10:42X:data:muEG 
 pwn_Update.py -n -g ReRecoMay10 queue=short.q,all.q
