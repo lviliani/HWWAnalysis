013e3c476d3dc329a425b36a6e68518157323d1a
thea <>
2011-08-03 10:14:08 +0000

    general purpose template

--- DileptonSelector/crab/prawn_template.sh
+++ DileptonSelector/crab/prawn_template.sh
@@ -11,7 +11,6 @@ source $VO_CMS_SW_DIR/cmsset_default.sh
 export SCRAM_ARCH=slc5_amd64_gcc434
 
 # CMSSW environment
-# cd $HOME/cmssw/HWWAnalysis/DileptonSelector
 cd $workingDir
 eval `scramv1 ru -sh`
 # DCAP environment (override CMSSW loaded libraries)
@@ -19,8 +18,7 @@ source $HOME/bin/rc/dcap.sh
 
 # cd $workingDir
 echo $PWD
-# ./bin/selectHWW.exe config/selectHWW_qsub.config -Selector.inputFile $inputFile -Selector.outputFile $outputFile -Selector.firstEvent $firstEvent -Selector.nEvents $nEvents
-cmsRun test/dileptonselector_cfg.py print inputFiles_load=$inputFile outputFile=$outputFile maxEvents=$nEvents skipEvents=$firstEvent $optArgs
+cmsRun $optArgs print inputFiles_load=$inputFile outputFile=$outputFile maxEvents=$nEvents skipEvents=$firstEvent
 
 # parameters provided by the framework:
 # $$sessionName  = $sessionName
