cc243642ba7195336edd21d0f7b3f51f2d938192
thea <>
2011-11-30 15:49:54 +0000

    path fixed

--- ShapeAnalysis/test/env.sh
+++ ShapeAnalysis/test/env.sh
@@ -2,7 +2,7 @@ SOURCE="${BASH_SOURCE[0]}"
 while [ -h "$SOURCE" ] ; do SOURCE="$(readlink "$SOURCE")"; done
 DIR="$( cd -P "$( dirname "$SOURCE" )" && pwd )"
 
-export PATH="$DIR/scripts:$PATH"
+export PATH="$DIR/../scripts:$PATH"
 export PYTHONPATH="$DIR/scripts:$PYTHONPATH"
 echo "$DIR/scripts added to PATH"
 export masses="110 115 120 130 140 150 160 170 180 190 200 250 300 350 400 450 500 550 600"
