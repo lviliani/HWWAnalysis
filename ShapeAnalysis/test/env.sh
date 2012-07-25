4b25fc8a6d13e2edcd7809406658bf66d57f6169
thea <>
2012-07-25 14:36:09 +0000

    python added to the PYTHONPATH

--- ShapeAnalysis/test/env.sh
+++ ShapeAnalysis/test/env.sh
@@ -1,9 +1,12 @@
 SOURCE="${BASH_SOURCE[0]}"
 while [ -h "$SOURCE" ] ; do SOURCE="$(readlink "$SOURCE")"; done
-DIR="$( cd -P "$( dirname "$SOURCE" )" && pwd )"
+SCRIPTS="$(dirname $SOURCE)/../scripts"
+PYTHON="$(dirname $SOURCE)/../python"
+DIRS="$( cd -P $SCRIPTS && pwd )"
+DIRP="$( cd -P $PYTHON && pwd )"
 
-export PATH="$DIR/../scripts:$PATH"
-export PYTHONPATH="$DIR/scripts:$PYTHONPATH"
-echo "$DIR/scripts added to PATH"
-export masses="110 115 120 130 140 150 160 170 180 190 200 250 300 350 400 450 500 550 600"
-export jets="0 1"
+export PATH="$DIRS:$PATH"
+export PYTHONPATH="$DIRS:$PYTHONPATH"
+echo "$DIRS added to PATH and PYTHONPATH"
+export PYTHONPATH="$DIRP:$PYTHONPATH"
+echo "$DIRP added to PATH"
