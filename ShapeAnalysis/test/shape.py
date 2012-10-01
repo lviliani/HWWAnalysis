f07338cd86ad02d14e3f14902afd710898602404
thea <>
2012-10-01 15:05:56 +0000

    updates shape.py example

---
+++ ShapeAnalysis/test/shape.py
@@ -0,0 +1,23 @@
+lumi=5.064
+rebin=10
+chans=['of_0j','of_1j','sf_0j','sf_1j']
+
+variable='mll'
+selection='bdt'
+range = 'mll'
+
+dataset='Data2012'
+mcset='0j1j'
+
+tag='mll_take2'
+xlabel='m(ll) [GeV]'
+
+# directories
+path_latino = '/shome/mtakahashi/HWW/Tree//ShapeAna/tree_skim_wwcommon'
+# path_bdt    = './bdt_skim_wwcommon/mva_MH{mass}_{category}'
+path_dd     = '/shome/thea/HWW/work/shape2012/data/bdt_2012_51fb/'
+
+# other directories
+path_shape_raw='raw'
+path_shape_merged='merged'
+
