1749587d8380e3508e048bcf083540262080b900
lara <>
2012-10-02 09:20:05 +0000

    shape.py with new mcset config

---
+++ ShapeAnalysis/test/examples/shape-mcset.py
@@ -0,0 +1,26 @@
+lumi=10.0
+rebin=10
+chans=['sf_0j','of_0j','of_1j','sf_1j']
+
+variable='mll'
+selection='bdt'
+#range='mllsplit'
+#split='mll'
+
+tag='mll_take1'
+xlabel='m_{ll}'
+dataset='Data2012'
+mcset='0j1j'
+
+# directories
+path_latino='/nfs/fanae/user/lara/ShapeAnalysisInputs/tree_skim_wwcommon'
+path_bdt='/nfs/fanae/user/lara/ShapeAnalysisInputs/bdt_skim_wwcommon'
+#path_dd='/nfs/fanae/user/lara/ShapeAnalysisInputs/dd/bdt_2012_51fb/'
+path_dd='/nfs/fanae/user/lara/ShapeAnalysisInputs/dd_lara/dd_10fb/dd/bdt_2012_51fb/dd_10fb/'
+#path_scale='/nfs/fanae/user/lara/archivosAless/shome/thea/HWW/ShapeAnalysis/data/datamcsf.{category}.txt'
+
+
+# other directories
+path_shape_raw='raw'
+path_shape_merged='merged'
+
