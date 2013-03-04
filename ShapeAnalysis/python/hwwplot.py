682002fb208635fe39eb729bb3b4732672eb04d6
thea <>
2013-03-04 14:44:18 +0000

    process sorting

--- ShapeAnalysis/python/hwwplot.py
+++ ShapeAnalysis/python/hwwplot.py
@@ -6,24 +6,24 @@ import logging
 import hwwtools
 import pdb
 
-_defaults  = {
-    'ggH':  { 'color':ROOT.kRed+1, 'label':'ggH', },
-    'vbfH': { 'color':ROOT.kRed+2, 'label':'qqH', },
-    'VH':   { 'color':ROOT.kRed+3, 'label':'VH' , },
-    'zH':   { 'color':ROOT.kRed-3, 'label':'zH' , },
-    'wH':   { 'color':ROOT.kRed-4, 'label':'wH' , },
+_defaults  = odict.OrderedDict([ 
+    ('ggH',  { 'color':ROOT.kRed+1, 'label':'ggH', }),
+    ('vbfH', { 'color':ROOT.kRed+2, 'label':'qqH', }),
+    ('wzttH',{ 'color':ROOT.kRed+3, 'label':'VH' , }),
+    ('VH',   { 'color':ROOT.kRed+3, 'label':'VH' , }),
+    ('zH',   { 'color':ROOT.kRed-3, 'label':'zH' , }),
+    ('wH',   { 'color':ROOT.kRed-4, 'label':'wH' , }),
 
-    'VV':   { 'color':858, 'label':'WZ/ZZ' , },  
-    'WJet': { 'color':921, 'label':'W+jets' , },
-    'Vg':   { 'color':617, 'label':'V+#gamma' , },
-    'VgS':  { 'color':618, 'label':'V+#gamma*' , },
-    'Top':  { 'color':400, 'label':'top' , },
-    'DYTT': { 'color':418, 'label':'DY+jets' , },
-    'DYLL': { 'color':419, 'label':'DY+jets' , },
-    'WW':   { 'color':851, 'label':'WW' , },
-    'ggWW': { 'color':853, 'label':'ggWW' , },
-
-}
+    ('VV',   { 'color':858, 'label':'WZ/ZZ' , }),  
+    ('DYTT', { 'color':418, 'label':'DY+jets' , }),
+    ('DYLL', { 'color':419, 'label':'DY+jets' , }),
+    ('Vg',   { 'color':617, 'label':'V+#gamma' , }),
+    ('VgS',  { 'color':618, 'label':'V+#gamma*' , }),
+    ('WJet', { 'color':921, 'label':'W+jets' , }),
+    ('Top',  { 'color':400, 'label':'top' , }),
+    ('WW',   { 'color':851, 'label':'WW' , }),
+    ('ggWW', { 'color':853, 'label':'WW' , }),
+])
 
 
 if not ROOT.gROOT.GetListOfClasses().FindObject('PlotVHqqHggH'):
@@ -38,25 +38,27 @@ class HWWPlot(ROOT.PlotVHqqHggH):
     
     _properties = ['label','color','scale','norm']
 
-    # enum
-    SIG = 1
-    BGK = 2
-
-    # rename
-
     #---
-    def __init__(self, defaults=_defaults):
+    def __init__(self, properties=_defaults):
         super(HWWPlot,self).__init__()
 
-        self._defaults = defaults
+        self._properties = properties
+        self._sortbydef = False
 
         self._data = None
         self._sigs = odict.OrderedDict()
         self._bkgs = odict.OrderedDict()
         self._errors = None
 
+    @property
+    def properties(self):
+        return self._properties
+
     def _add(self, name, coll, h, **kwargs):
         coll[name] = (h,kwargs) 
+
+    def setautosort(self, auto=True):
+        self._sortbydef = auto
 
     def seterror(self,eg):
         self._errors = eg
@@ -81,11 +83,19 @@ class HWWPlot(ROOT.PlotVHqqHggH):
             'norm'  : ROOT.vector('double')(),
             'th1'   : ROOT.vector('TH1*')(),
         }
+        
 
+        itproc = coll.iterkeys() if not self._sortbydef else self._properties.iterkeys()
+            
         # and fill them
-        for name,(h,props) in coll.iteritems():
+        for name in itproc:
+            try:
+                (h,props) = coll[name]
+            except KeyError:
+                # some of the processes might not be there
+                continue
             # 
-            dummy = self._defaults[name].copy()
+            dummy = self._properties[name].copy()
             dummy.update(props)
 
             vecs['th1']  .push_back(h)
