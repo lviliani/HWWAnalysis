eed281ac3957df4f1fed17900d0c07eaed4dbbac
thea <>
2013-04-01 08:55:07 +0000

    more colors, masking stdout

--- ShapeAnalysis/python/hwwplot.py
+++ ShapeAnalysis/python/hwwplot.py
@@ -11,29 +11,46 @@ _defaults  = odict.OrderedDict([
     ('vbfH', { 'color':ROOT.kRed+2, 'label':'qqH', }),
     ('wzttH',{ 'color':ROOT.kRed+3, 'label':'VH' , }),
     ('VH',   { 'color':ROOT.kRed+3, 'label':'VH' , }),
-    ('zH',   { 'color':ROOT.kRed-3, 'label':'zH' , }),
-    ('wH',   { 'color':ROOT.kRed-4, 'label':'wH' , }),
+    ('wH',   { 'color':ROOT.kRed-3, 'label':'wH' , }),
+    ('zH',   { 'color':ROOT.kRed-4, 'label':'zH' , }),
 
-    ('VV',   { 'color':858, 'label':'WZ/ZZ' , }),  
-    ('DYTT', { 'color':418, 'label':'DY+jets' , }),
-    ('DYLL', { 'color':419, 'label':'DY+jets' , }),
-    ('Vg',   { 'color':617, 'label':'V+#gamma' , }),
-    ('VgS',  { 'color':618, 'label':'V+#gamma*' , }),
-    ('WJet', { 'color':921, 'label':'W+jets' , }),
-    ('Top',  { 'color':400, 'label':'top' , }),
-    ('WW',   { 'color':851, 'label':'WW' , }),
-    ('ggWW', { 'color':853, 'label':'WW' , }),
+    ('VV',   { 'color':ROOT.kAzure-2,   'label':'WZ/ZZ'     , }),  
+    ('DYTT', { 'color':ROOT.kGreen+2,   'label':'DY+jets'   , }),
+    ('DYLL', { 'color':ROOT.kGreen+3,   'label':'DY+jets'   , }),
+    ('DYee', { 'color':ROOT.kGreen+3,   'label':'DY+jets'   , }),
+    ('DYmm', { 'color':ROOT.kGreen+3,   'label':'DY+jets'   , }),
+    ('Vg',   { 'color':ROOT.kMagenta+1, 'label':'V+#gamma'  , }),
+    ('VgS',  { 'color':ROOT.kMagenta+2, 'label':'V+#gamma*' , }),
+    ('WJet', { 'color':ROOT.kGray+1,    'label':'W+jets'    , }),
+    ('Top',  { 'color':ROOT.kYellow,    'label':'top'       , }),
+    ('ttbar',{ 'color':ROOT.kOrange-2,  'label':'t#bar{t}'  , }),
+    ('tW',   { 'color':ROOT.kOrange-4,  'label':'tW'        , }),
+    ('WW',   { 'color':ROOT.kAzure-9,   'label':'WW'        , }),
+    ('ggWW', { 'color':ROOT.kAzure-7,   'label':'WW'        , }),
 ])
 
 
 if not ROOT.gROOT.GetListOfClasses().FindObject('PlotVHqqHggH'):
     shape_path = os.path.join(os.getenv('CMSSW_BASE'),'src/HWWAnalysis/ShapeAnalysis')
-#     print 'Shape directory is',shape_path
 
-#     print 'Loading PlotVHqqHggH.C'
     hwwtools.loadAndCompile(shape_path+'/macros/PlotVHqqHggH.C')
 
+class NullStdOutSentry:
+    def __init__(self):
+
+        ignlvl = ROOT.gErrorIgnoreLevel
+        ROOT.gErrorIgnoreLevel = ROOT.kSysError
+        self.rdbuf = ROOT.std.cout.rdbuf()
+        ROOT.std.cout.rdbuf(0x0)
+        ROOT.gErrorIgnoreLevel = ignlvl
+
+    
+    def __del__(self):
+        ROOT.std.cout.rdbuf(self.rdbuf)
+
 class HWWPlot(ROOT.PlotVHqqHggH):
+
+
     _log = logging.getLogger('HWWPlot') 
     
     _properties = ['label','color','scale','norm']
@@ -49,6 +66,7 @@ class HWWPlot(ROOT.PlotVHqqHggH):
         self._sigs = odict.OrderedDict()
         self._bkgs = odict.OrderedDict()
         self._errors = None
+        self._verbose = False
 
     @property
     def properties(self):
@@ -107,8 +125,10 @@ class HWWPlot(ROOT.PlotVHqqHggH):
 
         return vecs
 
-    #---
+    # ---
     def prepare(self):
+
+        if not self._verbose: sentry = NullStdOutSentry()
 
         if self._data:   self.setDataHist(self._data)
         if self._errors: self.set_ErrorBand(self._errors)
@@ -126,6 +146,16 @@ class HWWPlot(ROOT.PlotVHqqHggH):
         self.set_vectColourBkg(vecBkg['color'])
 
         super(HWWPlot,self).prepare()
+
+    # ---
+    def draw(self,*args,**kwargs):
+        sentry = NullStdOutSentry()
+        super(HWWPlot,self).Draw(*args,**kwargs)
+    
+    # ---
+    def mergeSamples(self,*args,**kwargs):
+        sentry = NullStdOutSentry()
+        super(HWWPlot,self).mergeSamples(*args,**kwargs)
 
 if __name__ == '__main__':
     # 
