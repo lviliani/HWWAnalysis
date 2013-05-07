892adb7968979b1341818a44168f2a218061698e
amassiro <>
2013-05-07 09:13:34 +0000

    merging

--- ShapeAnalysis/scripts/mkDatacards.py
+++ ShapeAnalysis/scripts/mkDatacards.py
@@ -84,24 +84,31 @@ class ShapeDatacardWriter:
         card.write('bin'.ljust(58)+''.join([self._bin.ljust(coldef)*len(keyline)])+'\n')
         card.write('process'.ljust(58)+''.join([n.ljust(coldef) for (i,n,N) in keyline])+'\n' )
         card.write('process'.ljust(58)+''.join([('%d' % i).ljust(coldef) for (i,n,N) in keyline])+'\n' )
-        card.write('rate'.ljust(58)+''.join([('%-.3f' % N).ljust(coldef) for (i,n,N) in keyline])+'\n' )
+        card.write('rate'.ljust(58)+''.join([('%-.4f' % N).ljust(coldef) for (i,n,N) in keyline])+'\n' )
         card.write('-'*100+'\n')
 
 #         nmax = max([len(n) for n in nuisances]
 
         for name in nuisances:
             (pdf,effect) = nuisances[name]
-            if len(pdf) == 1: card.write('{0:<41} {1:<7}         '.format(name,pdf[0]))
-            else:             card.write('{0:<41} {1:<7} {2:<7}  '.format(name,pdf[0],pdf[1]))
+            # if this nuisance has no effect on any sample, not even write it!  --> "isAgoodNuisance"
+            isAgoodNuisance = False
             for i,p,y in keyline:
                 if p in effect:
-                    #if 'FakeRate' in name:
-                    if   (pdf[0]=='gmN'):                        card.write('%-10.5f'      % effect[p])
-                    elif (pdf[0]=='shape' or pdf[0]=='shapeN2'): card.write('%-10d'        % effect[p])
-                    elif (isinstance(effect[p], tuple)):         card.write('%.2f/%-5.2f' % (effect[p][0],effect[p][1]))
-                    else:                                        card.write('%-10.3f'      % effect[p])
-                else: card.write('-'.ljust(coldef))
-            card.write('\n')
+                  isAgoodNuisance = True
+
+            if (isAgoodNuisance) :
+               if len(pdf) == 1: card.write('{0:<41} {1:<7}         '.format(name,pdf[0]))
+               else:             card.write('{0:<41} {1:<7} {2:<7}  '.format(name,pdf[0],pdf[1]))
+               for i,p,y in keyline:
+                   if p in effect:
+                       #if 'FakeRate' in name:
+                       if   (pdf[0]=='gmN'):                        card.write('%-10.5f'      % effect[p])
+                       elif (pdf[0]=='shape' or pdf[0]=='shapeN2'): card.write('%-10d'        % effect[p])
+                       elif (isinstance(effect[p], tuple)):         card.write('%.2f/%-5.2f' % (effect[p][0],effect[p][1]))
+                       else:                                        card.write('%-10.3f'      % effect[p])
+                   else: card.write('-'.ljust(coldef))
+               card.write('\n')
 
         card.close()
 
@@ -219,8 +226,8 @@ class NuisanceMapBuilder:
         self._build()
  
     def _build(self):
-        # common 0/1 jet systematics
-        pureMC = [ 'VgS', 'Vg', 'VV', 'ggH', 'vbfH', 'wzttH', 'zH', 'wH', 'ttH', 'Other', 'VVV'] 
+        # common 0/1/2 jet systematics
+        pureMC = [ 'VgS', 'Vg', 'VV', 'ggH', 'qqH', 'wzttH', 'ZH', 'WH', 'ttH', 'Other', 'VVV', 'WW', 'WWewk']
         dummy = {}
         dummy['CMS_fake_e']    = (1.50, ['WJet']) # take the average of ee/me 
 #         dummy['CMS_fake_m']    = (1.42, ['WJet']) # take the average of mm/em
@@ -235,7 +242,7 @@ class NuisanceMapBuilder:
             self._common[k] = (['lnN'], dict([( process, v[0]) for process in v[1] ]) )
 
         self._common['pdf_gg']    = (['lnN'],dict([('ggWW',1.04),('ggH',1.08)]) )
-        self._common['pdf_qqbar'] = (['lnN'],dict([('WW',1.04),('VV',1.04),('vbfH',1.02)]) )
+        self._common['pdf_qqbar'] = (['lnN'],dict([('WW',1.04),('VV',1.04),('qqH',1.02)]) )
         self._common['pdf_assoc'] = (['lnN'],dict([('WW',1.04)]) )
 
         dummy = {} 
@@ -245,8 +252,8 @@ class NuisanceMapBuilder:
         dummy['QCDscale_ggH1in']        = ([0.89, 1.39], ['ggH'])
         dummy['QCDscale_ggH_ACEPT']     = ([1.02, 1.02], ['ggH'])
         dummy['QCDscale_ggVV']          = ([1.30, 1.30], ['ggWW'])
-        dummy['QCDscale_qqH']           = ([1.01, 1.01], ['vbfH'])
-        dummy['QCDscale_qqH_ACEPT']     = ([1.02, 1.02], ['vbfH'])
+        dummy['QCDscale_qqH']           = ([1.01, 1.01], ['qqH'])
+        dummy['QCDscale_qqH_ACEPT']     = ([1.02, 1.02], ['qqH'])
         dummy['QCDscale_wzttH_ACEPT']   = ([1.02, 1.02], ['wzttH'])
         dummy['QCDscale_wzttH']         = ([1.01, 1.01], ['wzttH'])
         dummy['UEPS']                   = ([0.94, 1.11], ['ggH'])
@@ -272,7 +279,7 @@ class NuisanceMapBuilder:
             self._1jetOnly[k] = (['lnN'], dict([( process, v[0]) for process in v[1] ]) )
 
 
-    def _addDataDrivenNuisances(self, nuisances, yields, mass, channel, jetcat, suffix=''):
+    def _addDataDrivenNuisances(self, nuisances, yields, mass, channel, jetcat, suffix, opts):
 
         if self._ddreader.iszombie: return
         (estimates,dummy) = self._wwddfilter.get(mass, channel)
@@ -292,6 +299,14 @@ class NuisanceMapBuilder:
             'DYee' : ( channel+cb, ['DYee']      ),
             'DYmm' : ( channel+cb, ['DYmm']      ),
         }
+
+        # see if there are MC extrapolation scale factors needed
+        MCextrapFile = 'ScaleFactors.py'
+        if os.path.exists(MCextrapFile):
+            handle = open(MCextrapFile,'r')
+            scaleFactor = {}
+            exec(handle)
+            handle.close()
 
         eff_bin1_tmpl = 'CMS{0}_hww_{1}_{2}_stat_bin1'
         for tag,(context, processes) in mapping.iteritems(): 
@@ -322,6 +337,22 @@ class NuisanceMapBuilder:
 
             for process in available:
                 e = estimates[process]
+
+                doneMCextrap = False
+                if 'MCextrap' in opts:
+                  # MC extrapolation is needed for some dd
+                  for nameNuis,nameNominal,nameSimilTemplate in opts['MCextrap'] :
+                    # check if the name of the nuisance is correct
+                    #print "nameNuis = ",nameNuis,"   :: eff_stat = ",eff_stat
+                    if (nameNuis == eff_stat) :
+                      # read from file the scale factor
+                      # e.g.: scaleFactor.update({110: {'CHITOP-Top': 0.02029}})
+                      #       MCextrap = [('CMS_8TeV_hww_Top_2j_stat',  'Top',      'CHITOP')]
+                      valueScaleFactor = scaleFactor[mass]['%s-' %nameSimilTemplate + '%s' %nameNominal]
+                      #print valueScaleFactor
+                      e.alphaPrime = e.alpha * valueScaleFactor
+                      doneMCextrap = True
+
                 extrUnc = 1+e.delta/e.alpha if pdf != 'gmM' else e.delta/e.alpha
 
                 if e.deltaUnCorr != 0:
@@ -333,11 +364,19 @@ class NuisanceMapBuilder:
 #                          extr_corr_entries[process] = 1. + e.deltaCorr/e.alpha
 #                          flagdoextracorr = 1
                 extr_entries[process] = extrUnc
-                stat_entries[process] = e.alpha
+                if (e.alphaPrime < 0) :
+                  stat_entries[process] = e.alpha
+                else :
+                  stat_entries[process] = e.alphaPrime  # in case of MC extrapolation
+
                 eff_bin1 = eff_bin1_tmpl.format(suffix,process,channel)
-                if eff_bin1 in nuisances:
+                if eff_bin1 in nuisances and not doneMCextrap:
                     del nuisances[eff_bin1]
 
+                if not doneMCextrap and 'CMS%s' % suffix +'_eff_e' in nuisances and process == 'Top':
+                  del nuisances['CMS%s' % suffix +'_eff_e'][1][process]
+                if not doneMCextrap and 'CMS%s' % suffix +'_eff_m' in nuisances and process == 'Top':
+                  del nuisances['CMS%s' % suffix +'_eff_m'][1][process]
 
 #             if jetcat == '2j' and tag == 'Top' :
 #                 if flagdoextracorr == 1 :
@@ -390,7 +429,7 @@ class NuisanceMapBuilder:
             nuisances[tag] = (['shapeN2'],dict([ (p,1) for p in processes]) )
 
 
-    def _addExperimentalShapeNuisances(self, nuisances, effects, suffix=''):
+    def _addExperimentalShapeNuisances(self, nuisances, effects, suffix, yields):
         '''Experimental Shape-based nuisances'''
         # expr for CMS nuisances
         expRegex  = re.compile('CMS_(.+)')
@@ -414,14 +453,21 @@ class NuisanceMapBuilder:
             if tag in nuisances: del nuisances[tag]
           # nuisances[tag] = (['shapeN2'],dict([ (p,1) for p in effects[eff] if p not in self.expShapeVeto[tag]]) )
           # nuisances[tag] = (['shapeN2'],dict([ (p,1) for p in effects[eff] if p not in self.expShapeVeto.get(tag,[])]) )
-            nuisances[tag] = (['shapeN2'],dict([ (p,1) for p in effects[eff] if (not (tag in self.expShapeVeto) or p not in self.expShapeVeto[tag]) ]) )
+          #  nuisances[tag] = (['shapeN2'],dict([ (p,1) for p in effects[eff] if (not (tag in self.expShapeVeto) or p not in self.expShapeVeto[tag]) ]) )
+          #  nuisances[tag] = (['shapeN2'],dict([ (p,1) for p in effects[eff] if ((not (tag in self.expShapeVeto) or p not in self.expShapeVeto[tag])) and ( yields[p] is not None and yields[p]._N()!=0 )  ]) )
+            names = []
+            for tempnames, tempvalue in yields.iteritems():
+              names.append(tempnames)
 
-    def _addShapeNuisances(self, nuisances, effects, opts, suffix=''):
+          # remove nuisance if 0 entries in nominal! "Bogus norm" error in combine
+            nuisances[tag] = (['shapeN2'],dict([ (p,1) for p in effects[eff] if ((not (tag in self.expShapeVeto) or p not in self.expShapeVeto[tag])) and ( p in names and yields[p]._N!=0 )  ]) )
+
+    def _addShapeNuisances(self, nuisances, effects, opts, suffix, yields):
         # local copy
         shapeNu = OrderedDict()
 
         self._addWWShapeNuisances(shapeNu, effects)
-        self._addExperimentalShapeNuisances(shapeNu, effects, suffix)
+        self._addExperimentalShapeNuisances(shapeNu, effects, suffix, yields)
 
         if 'shapeFlags' not in opts:
             sys.exit(-1)
@@ -486,10 +532,10 @@ class NuisanceMapBuilder:
 
 
         self._addStatisticalNuisances(allNus, yields, channel, suffix)
-        self._addDataDrivenNuisances(allNus, yields, mass, channel, jetcat, suffix)
+        self._addDataDrivenNuisances(allNus, yields, mass, channel, jetcat, suffix, opts)
 
         if self._shape:
-            self._addShapeNuisances(allNus,effects, opts, suffix)
+            self._addShapeNuisances(allNus,effects, opts, suffix, yields)
 
         if 'nuisFlags' not in opts:
             raise RuntimeError('nuisFlags not found among the allNus options')
@@ -518,6 +564,9 @@ def incexc(option, opt_str, value, parser):
         setattr(parser.values,'shapeFlags',[])
     if not hasattr(parser.values,'nuisFlags'):
         setattr(parser.values,'nuisFlags',[])
+
+    if not hasattr(parser.values,'MCextrap'):
+        setattr(parser.values,'MCextrap',[])
 
     optarray = str(option).split('/')
     print optarray
@@ -548,6 +597,7 @@ if __name__ == '__main__':
     parser.add_option('--cutbased'      , dest='shape'       , help='Make cutbased datacards (no shapes)' , default=True , action='store_false' )
     parser.add_option('--no_wwdd_above' , dest='noWWddAbove' , help='No WW dd above this mass'            , default=None   , type='int'     )
     parser.add_option('--dataset'       , dest='dataset'     , help='Dataset to process'                  , default=None)
+    parser.set_defaults(MCextrap=[])
     parser.set_defaults(shapeFlags=[])
     parser.set_defaults(nuisFlags=[])
     parser.add_option('--Xsh','--excludeShape', dest='shapeFlags'        , help='exclude shapes nuisances matching the expression', action='callback', type='string', callback=incexc)
@@ -559,6 +609,7 @@ if __name__ == '__main__':
 #     parser.add_option('--floatN',               dest='floatN'            , help='float normalisation of particular processes, separate by space ', default=' ')
     parser.add_option('--isssactive',           dest='isssactive'        , help='Is samesign datacard available'                           , default=False)
     parser.add_option('--floatN',               dest='floatN'            , help='float normalisation of particular processes, separate by space',  default=[] , type='string' , action='callback' , callback=hwwtools.list_maker('floatN'))
+
     hwwtools.addOptions(parser)
     hwwtools.loadOptDefaults(parser)
 
@@ -568,6 +619,7 @@ if __name__ == '__main__':
     print 'noWWddAbove:',opt.noWWddAbove
     print 'dataset:    ',opt.dataset
     print 'isssactive: ',opt.isssactive
+    print 'MCextrap:   ',opt.MCextrap
 
     # checks
     if not opt.variable or not opt.lumi:
@@ -604,6 +656,7 @@ if __name__ == '__main__':
     os.symlink(os.path.abspath(mergedPath), shapeDir)
 
     optsNuis = {}
+    optsNuis['MCextrap'] = opt.MCextrap
     optsNuis['shapeFlags'] = opt.shapeFlags
     optsNuis['nuisFlags'] = opt.nuisFlags
     optsNuis['floatN'] = opt.floatN
@@ -639,7 +692,8 @@ if __name__ == '__main__':
 
             # reshuffle the order
             #order = [ 'vbfH', 'ggH', 'wzttH', 'ggWW', 'Vg', 'WJet', 'Top', 'WW', 'DYLL', 'VV', 'DYTT', 'Data']
-            order = [ 'ggH', 'ggH_ALT', 'vbfH','vbfH_ALT' , 'wzttH','wzttH_ALT', 'wH', 'zH', 'ttH', 'ggWW', 'VgS', 'Vg', 'WJet', 'Top', 'WW', 'DYLL', 'VV', 'DYTT', 'DYee', 'DYmm', 'Other', 'ggH125', 'vbfH125', 'wzttH125', 'VVV', 'Data']
+            order = [ 'ggH','ggH_ALT','qqH','qqH_ALT', 'wzttH','wzttH_ALT', 'WH', 'ZH', 'ttH', 'ggWW', 'VgS', 'Vg', 'WJet', 'Top', 'WW', 'WWewk', 'DYLL', 'VV', 'DYTT', 'DYee', 'DYmm', 'Other', 'ggH125', 'qqH125', 'wzttH125', 'VVV', 'Data']
+
             oldYields = yields.copy()
             yields = OrderedDict([ (k,oldYields[k]) for k in order if k in oldYields])
             
@@ -652,8 +706,8 @@ if __name__ == '__main__':
             for n,(pdf, eff) in nuisances.iteritems():
                 if 'ggH' in eff and 'shape' not in pdf[0] and 'stat_bin' not in n :
                     eff['ggH_ALT'] =  eff['ggH']
-                if 'vbfH' in eff and 'shape' not in pdf[0] and 'stat_bin' not in n :
-                    eff['vbfH_ALT'] =  eff['vbfH']
+                if 'qqH' in eff and 'shape' not in pdf[0] and 'stat_bin' not in n :
+                    eff['qqH_ALT'] =  eff['qqH']
                 if 'wzttH' in eff and 'shape' not in pdf[0] and 'stat_bin' not in n :
                     eff['wzttH_ALT'] =  eff['wzttH']
 
