7471b189e20854563430f346e3ea9a3e5398974e
Daniel Meister <dmeister@phys.ethz.ch>
2014-03-25 15:31:35 +0100

    apply trigger efficiencies bin-by-bin

--- ShapeAnalysis/python/triggerEffCombiner.py
+++ ShapeAnalysis/python/triggerEffCombiner.py
@@ -6,53 +6,62 @@ from math import *
 from pprint import pprint
 
 
-triggers = []
+triggers = {}
 
 class TriggerEff:
 
     def __init__(self,path):
         
         # open file (TODO: replace with config option)
-        fits = open(path, "r")
+        vals = open(path, "r")
 
         # read every line
-        for line in fits:
-            m = re.match(r"2012[ABC]?_(\d+)-(\d+)_(\w+)_(barrel|endcap|forward|absetabin[0123])\s+.*", line)
+        for line in vals:
+            m = re.match(r"(Single|Double)(Mu|El)(Lead|Trail)?\s([0-9\.]+)\s([0-9\.]+)\s([0-9\.]+)\s([0-9\.]+)\s([0-9\.]+)", line)
             if not m: continue
-            numbers = [float(x) for x in line.split()[1:]]
-            (start, end) = int(m.group(1)), int(m.group(2))
-            trigname = m.group(3)
-            etarange = m.group(4)
-            found = False
-            for (trigname2,start2,end2,pars) in triggers:
-                if trigname2 == trigname and start2 == start and end2 == end:
-                    pars[etarange] = numbers
-                    found = True
-                    break
-            if not found:
-                triggers.append((trigname,start,end,{etarange:numbers}))
+            if (m.group(1) == 'Single'):
+                if (m.group(2) == 'El'):
+                    trigname = 'SingleEl'
+                else:
+                    trigname = 'SingleMu'
+            else:
+                if (m.group(2) == 'El'):
+                    trigname = 'Ele17_Ele8_Ele'
+                else:
+                    trigname = 'Mu17_Mu8_Mu'
+                if (m.group(3) == 'Lead'):
+                    trigname += '17Leg'
+                else:
+                    trigname += '8Leg'
+            pt = (float(m.group(6)),float(m.group(7)))
+            eta = (float(m.group(4)),float(m.group(5)))
+            eff = float(m.group(8))
+            if not trigname in triggers:
+                triggers[trigname] = []
+            triggers[trigname].append({'ptrange': pt, 'etarange': eta, 'efficiency': eff})
 
+#        print triggers
+#        exit(0)
 
     def getMMeff(self):
         # works only with 2012 data for the moment
         triggers = self.getTriggersForRun(190456)
-        return DiMuEventEfficiency(triggers)
+        return DiMuEventEfficiency(triggers,mode='onlyDouble')
 
     def getEEeff(self):
         # works only with 2012 data for the moment
         triggers = self.getTriggersForRun(190456)
-        return DiElEventEfficiency(triggers)
+        return DiElEventEfficiency(triggers,mode='onlyDouble')
 
     def getEMeff(self):
         # works only with 2012 data for the moment
         triggers = self.getTriggersForRun(190456)
-        return ElMuEventEfficiency(triggers)
+        return ElMuEventEfficiency(triggers,mode='onlyDouble')
 
     def getTriggersForRun(self, run, verbose=0):
         ret = {}
-        for (trigname,start,end,pars) in triggers:
-            if run >= start and run <= end:
-                ret[trigname] = LegEfficiency(trigname,pars)
+        for (trigname,pars) in triggers.items():
+            ret[trigname] = LegEfficiency(trigname,pars)
         if verbose: print "Triggers for run %d: %s" % (run, ", ".join(ret.keys()))
         return ret
 
@@ -80,42 +89,27 @@ class TriggerEff:
 class LegEfficiency:
     def __init__(self,name,params):
         self._name    = name
-        self._isMu    = ("Mu" in name)
-        self._isSingleMu = ("SingleMu" in name)
-        if self._isSingleMu:
-            self._plateau = [params['absetabin0'][0], params['absetabin1'][0], params['absetabin2'][0], params['absetabin3'][0]]
-            self._turnon  = [params['absetabin0'][1], params['absetabin1'][1], params['absetabin2'][1], params['absetabin3'][1]]
-            self._resol   = [params['absetabin0'][2], params['absetabin1'][2], params['absetabin2'][2], params['absetabin3'][2]]
-            self._ptcut   = [params['absetabin0'][3], params['absetabin1'][3], params['absetabin2'][3], params['absetabin3'][3]]
-            self._plat2   = [params['absetabin0'][4], params['absetabin1'][4], params['absetabin2'][4], params['absetabin3'][4]]            
-        elif self._isMu:
-            self._plateau = [params['barrel'][0], params['endcap'][0], params['forward'][0]]
-            self._turnon  = [params['barrel'][1], params['endcap'][1], params['forward'][1]]
-            self._resol   = [params['barrel'][2], params['endcap'][2], params['forward'][2]]
-            self._ptcut   = [params['barrel'][3], params['endcap'][3], params['forward'][3]]
-            self._plat2   = [params['barrel'][4], params['endcap'][4], params['forward'][4]]            
-        else:
-            self._plateau = [params['barrel'][0], params['endcap'][0]]
-            self._turnon  = [params['barrel'][1], params['endcap'][1]]
-            self._resol   = [params['barrel'][2], params['endcap'][2]]
-            self._ptcut   = [params['barrel'][3], params['endcap'][3]]
-            self._plat2   = [params['barrel'][4], params['endcap'][4]]
+        self._params  = params
     def __call__(self,pt,eta,verbose=0):
-        ieta = 0
-        if self._isSingleMu:
-            if abs(eta) > 0.8: ieta = 1
-            if abs(eta) > 1.2: ieta = 2
-            if abs(eta) > 2.1: ieta = 3
-        elif self._isMu:
-            if abs(eta) > 1.2: ieta = 1
-            if abs(eta) > 2.1: ieta = 2
-        else:
-            if abs(eta) > 1.5: ieta = 1
-        ret = self._plat2[ieta]
-        if pt <= self._ptcut[ieta] and self._ptcut[ieta] > 8:
-            ret = self._plateau[ieta]/(1 + exp(-(pt - self._turnon[ieta])/self._resol[ieta]))
-        if verbose > 0: print  "      %s: for (pt,eta) = (%.1f,%.2f), efficiency is %5.2f%%" % (self._name,pt,eta,ret*100)
-        return ret
+        for bin in self._params:
+            (ptlow,pthigh) = bin['ptrange']
+            (etalow,etahigh) = bin['etarange']
+            if pt >= ptlow and pt < pthigh and abs(eta) >= etalow and abs(eta) < etahigh:
+                # no linear interpolation
+                return bin['efficiency']
+                # comment out above line to run with linear interpolation in pt
+                ptcen = (ptlow + pthigh) / 2
+                eff = bin['efficiency']
+                for nbin in self._params:
+                    (nptlow,npthigh) = nbin['ptrange']
+                    (netalow,netahigh) = nbin['etarange']
+                    if (pt > ptcen and nptlow == pthigh or pt <= ptcen and npthigh == ptlow) and netalow == etalow and netahigh == etahigh:
+                        nptcen = (nptlow + npthigh) / 2
+                        neff = nbin['efficiency']
+                        return eff + (pt - ptcen) * (neff - eff) / (nptcen - ptcen)
+                return eff
+        print pt, eta
+        return 0.0
 
 class ZeroEfficiency:
     def __init__(self, name=None):
@@ -158,7 +152,7 @@ class DiMuEventEfficiency:
         q0 = effs_leg2[0]
         q1 = effs_leg2[1]
         q2 = effs_leg2[2]
-        eff = p0 + (1-p0)*q0 + (1-p0)*(1-q0)*(p1*q2 + (1-p1*q2)*p2*q1)
+        eff = p0 * (1 - q2) + q0 * (1 - p2) + p1 * q2 + p2 * q1 - p1 * q1
         return eff
 
 class DiElEventEfficiency:
@@ -168,9 +162,9 @@ class DiElEventEfficiency:
             self._singleEl = legs["SingleEl"] if "SingleEl" in legs else ZeroEfficiency("NoSingleEl")
         else:
             self._singleEl = ZeroEfficiency()
-        if "Ele17_Ele8_Ele17Leg_fromEle8" in legs:
-            self._doubleElHighPt = legs["Ele17_Ele8_Ele17Leg_fromEle8"]
-            self._doubleElLowPt  = legs["Ele17_Ele8_Ele8Leg_fromEle8"]
+        if "Ele17_Ele8_Ele17Leg" in legs:
+            self._doubleElHighPt = legs["Ele17_Ele8_Ele17Leg"]
+            self._doubleElLowPt  = legs["Ele17_Ele8_Ele8Leg"]
         else:
             self._doubleElHighPt = ZeroEfficiency("NoDoubleEl_HighPt")
             self._doubleElLowPt  = ZeroEfficiency("NoDoubleEl_LowPt") 
@@ -183,7 +177,7 @@ class DiElEventEfficiency:
         q0 = effs_leg2[0]
         q1 = effs_leg2[1]
         q2 = effs_leg2[2]
-        eff = p0 + (1-p0)*q0 + (1-p0)*(1-q0)*(p1*q2 + (1-p1*q2)*p2*q1)
+        eff = p0 * (1 - q2) + q0 * (1 - p2) + p1 * q2 + p2 * q1 - p1 * q1
         return eff
 
 class ElMuEventEfficiency:
@@ -204,9 +198,9 @@ class ElMuEventEfficiency:
         else:
             self._doubleMuHighPt = ZeroEfficiency()
             self._doubleMuLowPt  = ZeroEfficiency() 
-        if "Ele17_Ele8_Ele17Leg_fromEle8" in legs:
-            self._doubleElHighPt = legs["Ele17_Ele8_Ele17Leg_fromEle8"]
-            self._doubleElLowPt  = legs["Ele17_Ele8_Ele8Leg_fromEle8"]
+        if "Ele17_Ele8_Ele17Leg" in legs:
+            self._doubleElHighPt = legs["Ele17_Ele8_Ele17Leg"]
+            self._doubleElLowPt  = legs["Ele17_Ele8_Ele8Leg"]
         elif "Ele17_Ele8_Tighter_Ele17Leg_fromEle8" in legs:
             self._doubleElHighPt = legs["Ele17_Ele8_Tighter_Ele17Leg_fromEle8"]
             self._doubleElLowPt  = legs["Ele17_Ele8_Tighter_Ele8Leg_fromEle8"]
@@ -222,7 +216,7 @@ class ElMuEventEfficiency:
         q0 = effs_leg2[0]
         q1 = effs_leg2[1]
         q2 = effs_leg2[2]
-        eff = p0 + (1-p0)*q0 + (1-p0)*(1-q0)*(p1*q2 + (1-p1*q2)*p2*q1)
+        eff = p0 * (1 - q2) + q0 * (1 - p2) + p1 * q2 + p2 * q1 - p1 * q1
         return eff
 
 class LuminosityWeightedAverage:
