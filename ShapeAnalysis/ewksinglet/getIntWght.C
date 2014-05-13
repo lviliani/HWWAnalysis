c7d29132bfe62cf28d0d319f20487b0c9ff56a4b
Andrea <massironi.andrea@gmail.com>
2014-05-13 18:10:44 +0200

    update qqH - fix

--- ShapeAnalysis/ewksinglet/getIntWght.C
+++ ShapeAnalysis/ewksinglet/getIntWght.C
@@ -248,9 +248,6 @@ void initIntWght(std::string wFile , int iType , int iSyst, float Hmass = 350, f
    }
    else if ( iType ==1 ) { //---- qqH
 
-    ,1.0); //---- 0 is 
-       
-    
     TString *readfile = new TString ("data/InterferenceVBF/file_for_interpolation.root"); //file with the values of the all parameters
     TFile* SI = new TFile(readfile->Data());
     Double_t fill_param[16]; // 9 + 7 = 16
